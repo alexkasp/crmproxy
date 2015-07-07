3aa654e451c38b8c377318fed1e26defd3f63b50
root <root@msk6.sipuni.com>
2015-07-07 12:39:54 +0300

    add include to git

---
+++ main.cpp
@@ -0,0 +1,40 @@
+//
+//  main.cpp
+//  crmproxy
+//
+//  Created by Александр Салихов on 24.06.15.
+//
+//
+
+#include <iostream>
+#include <EventReader.h>
+#include <CRMUrlBuilder.h>
+#include <Parser.h>
+#include <RegisterMonitor.h>
+
+using namespace std;
+
+int main()
+{
+    
+    
+    EventReader reader("127.0.0.1",5038);
+    
+    CRMUrlBuilder sender;
+    Parser newParser("/ext/crm_api/pbxCrmGatewayHandler?userId=");
+    
+    RegisterParser rparser("not need");
+    RegisterMonitor monitor(&rparser,"/var/lib/asterisk/agi-bin/system_variables.php");
+    
+    sender.AddParser(&newParser);
+    
+    
+    reader.AddExecuter(&sender);
+    
+    reader.AddExecuter(&monitor);
+    
+    reader.start();
+    
+    
+    return 0;
+}
