e6eadb1733e25d3a9d578a895dec696d5cda3ba0
alexkasp <a.salihov@sipuni.com>
2015-09-23 19:29:49 +0300

    Add ICMServer

--- icmserver/ICMServer.cpp
+++ icmserver/ICMServer.cpp
@@ -1,1 +1,57 @@
 #include <ICMServer.h>
+
+ICMServer::ICMServer(int port):socket(io_serice)
+{
+    
+}
+
+int ICMServer::init(int port)
+{
+    socket.open(ip::udp::v4();
+    boost::system::error_code ec;
+    socket.bind(ip::udp::endpoint(ip::udp::v4(),port),ec);
+        
+    if(!ec)
+        return 1;
+    return 0;
+}
+            
+void ICMServer::startProcessing()
+{
+    boost::thread t1(boost::bind(&ICMServer::prepareAccept,this));
+    t1.detach();
+}
+
+void ICMServer::prepareAccept()
+{
+    boost::shared_ptr<boost::asio::streambuf> buf = boost::shared_ptr<boost::asio::streambuf>(new boost::asio::streambuf());
+    
+    boost::shared_ptr<ip::udp::endpoint> sender = boost::shared_ptr<ip::udp::endpoint>(new ip::udp::endpoint());
+    socket.async_receive(boost::asio::buffer(*buf),*sender,boost::bind(&ICMServer::pro));
+    io_serice.run();
+}
+
+void ICMServer::processICM(const boost::system::error_code& error,std::size_t bytes_transferred, boost::shared_ptr<ip::udp::endpoint> sender,boost::shared_ptr<boost::asio::streambuf> databuf)
+{
+    
+    if(!error)
+    {
+        prepareAccept();
+        
+        boost::asio::streambuf& buf = *databuf;
+        ip::udp::endpoint sender_ept = *sender;
+        
+        string str(boost::asio::buffers_begin(buf.data()), boost::asio::buffers_begin(buf.data()) + buf.size());
+        
+        buf.consume(size);
+        
+        boost::thread solveThread(boost::bind(&ICMServer::solveRequest,this,str,sender_ept));
+        
+    }
+}
+
+                void ICMServer::solveRequest(string number,boost::shared_ptr<ip::udp::endpoint> sender)
+                {
+                    string icmMSG = "not found\r\n";
+                    socket.send_to(boost::asio::buffer(icmMSG),*sender);
+                }
--- include/ICMServer.h
+++ include/ICMServer.h
@@ -1,7 +1,25 @@
 #ifndef ICM_SERVER
 #define ICM_SERVER
 
+#include <boost/asio.hpp>
+#include <boost/thread.hpp>
+#include <string>
+
+using namespace boost::asio;
+using namespace std;
+
 class ICMServer
-{};
+{
+    io_service service;
+    ip::udp::socket socket;
+    
+    void prepareAccept();
+    void processICM(const boost::system::error_code& error,std::size_t bytes_transferred, boost::shared_ptr<ip::udp::endpoint> sender,boost::shared_ptr<boost::asio::streambuf> databuf);
+    void solveRequest(string number,boost::shared_ptr<ip::udp::endpoint> sender);
+public:
+    int init(int port);
+    void startProcessing();
+    
+};
 
 #endif
